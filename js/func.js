eeb12302378d316404e92d0fa5ed416cd54a32b0
github <353556243@qq.com>
2019-10-18 21:06:50 +0800

    Site updated: 2019-10-18 21:06:49

--- js/func.js
+++ js/func.js
@@ -31,13 +31,23 @@ var fnTextPopup = function(arr, options) {
 
 fnTextPopup(['CSS', 'HTML', 'JavaScript', 'jQuery', 'Php', 'Node.js', 'Vue', '法治', '爱国', '敬业', '诚信', '友善']);
 
- var text = document.title
-        var timerId
+var text = document.title
+var timerId
 
-        function newtext() {
-            clearTimeout(timerId)
-            document.title = text.substring(1, text.length) + text.substring(0, 1)
-            text = document.title.substring(0, text.length)
-            timerId = setTimeout("newtext()", 1500)
-        }
-        newtext();
+function newtext() {
+    clearTimeout(timerId)
+    document.title = text.substring(1, text.length) + text.substring(0, 1)
+    text = document.title.substring(0, text.length)
+    timerId = setTimeout("newtext()", 1500)
+}
+newtext();
+
+
+
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?456664e494b645c9b0b0d53341da9136";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
